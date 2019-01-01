f1d108617c94f7386c29169865c74a63c9e61c8f
KrishnaKumar <meetkrishnasuresh@gmail.com>
2019-01-01 21:21:43 +0530

    Initial Commit

---
+++ api/station-api-maven/src/main/java/com/kk/api/station/StationApiApplication.java
@@ -0,0 +1,14 @@
+package com.kk.api.station;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StationApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StationApiApplication.class, args);
+	}
+
+}
+
