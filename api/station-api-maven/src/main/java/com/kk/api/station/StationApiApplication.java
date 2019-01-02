e54833e176a579b1117e65fb4baa83f33f8bca98
KrishnaKumar <meetkrishnasuresh@gmail.com>
2019-01-02 06:47:37 +0530

    Added StationDBModel for Hibernate Model

--- api/station-api-maven/src/main/java/com/kk/api/station/StationApiApplication.java
+++ api/station-api-maven/src/main/java/com/kk/api/station/StationApiApplication.java
@@ -2,8 +2,10 @@ package com.kk.api.station;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.kk.api.station"})
 public class StationApiApplication {
 
 	public static void main(String[] args) {
