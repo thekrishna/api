694de044d25a7ce10d7d1c685b0ba17d61a76629
KrishnaKumar <meetkrishnasuresh@gmail.com>
2019-01-04 09:55:15 +0530

    Added Swagger annotation

--- station-api-maven/src/main/java/com/kk/api/station/StationApiApplication.java
+++ station-api-maven/src/main/java/com/kk/api/station/StationApiApplication.java
@@ -4,8 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @SpringBootApplication
 @ComponentScan(basePackages = {"com.kk.api.station"})
+@EnableSwagger2
 public class StationApiApplication {
 
 	public static void main(String[] args) {
